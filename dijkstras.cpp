38dc5f5d47d9ded57bedc1d23151d93b853566a7
SebasCodingCo <sebmanjr@icloud.com>
2024-10-23 19:41:19 -0400

    Update dijkstras.cpp
    improved dijkstras still not complete though

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -22,31 +22,42 @@ map<int,int> dijkstra(map<int, vector<int>> graph, vector<int> vals, int node){
     //the tuple stores trios of cost, name, and the 
     //looked up how to make a priority queue a min heap on geeks for geeks and made it hold tuples
     priority_queue<tuple<int, int, int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> frontier;
-    //marked stores the node and the cost
+    //marked stores the node and the cost to get there, realized we need the cost to find the smallest path
     map<int, int> marked;
+    //path should store the node and who it is coming from
+    map<int,int> path;
     //create the first tuple in frontier so we can just continuously go through the 
     //while loop until the shortest path to each is found
     frontier.push(make_tuple(0, node, node));
+    path[0] = 0;
 
     while(!frontier.empty()){
-	       
         tuple <int,int,int> top = frontier.top();
         node = get<1>(top);
         frontier.pop();
-        //continue if node is not in marked
+        //continue if node is not in marked 
         if(marked.count(node) == 0){
-            //vals[node] is the cost of going into the current node
-            
-            marked[node] = get<2>(top);
-            for(size_t i = 0; i < graph[node].size(); i ++){
+            //vals[node] is the cost of getting into the current node            
+            marked[node] = get<0>(top);
+            for(int u: graph[node]){
+                //need a conditional because we don't want to add to a value that has already unless it is a shorter path
                 //graph[node][i] is the node connecting to current node
                 //vals[graph[node][i]] is the cost of the node connecting to the current node 
                 //frontier push tuple (u.cost + v.cost, v.name, u.name)
-                frontier.push(make_tuple(vals[graph[node][i]] + vals[node], graph[node][i], node));
+                if(marked.count(u) == 0){
+                    frontier.push(make_tuple(vals[u] + marked[node], u, node));
+                    path[u] = node;
+                }
+                else if((vals[u] + marked[node]) < marked[u]){
+                    frontier.push(make_tuple(vals[u] + marked[node], u, node));       
+                    path[u] = node;            
+                }
+                
             }
         }
     }
-    return marked;
+    
+    return path;
 }
 
 int main(int argc, char *argv[]) {
@@ -105,7 +116,7 @@ int main(int argc, char *argv[]) {
     int row1,column1,row2,column2;
     cin >> row1 >> column1;
     //row1*rows +columns is where our beginning node should be
-    map<int,int> pathCost = dijkstra(graphList,mapVals, (row1*rows + column1));
+    map<int,int> path = dijkstra(graphList,mapVals, (row1*rows + column1));
     cin >> row2 >> column2;
 
     //could be row2*columns need to check
@@ -114,14 +125,17 @@ int main(int argc, char *argv[]) {
     //go until row2 column2 reaches the original node
     map<int,int> :: iterator i;
 	//go through every piece of information going from artist down to song
-	for(i = pathCost.begin(); i != pathCost.end(); i++){
+	for(i = path.begin(); i != path.end(); i++){
         cout << i->first << ":" << i->second << endl;
 	}
+
     int totalCost = 0;
     int currentNode = endNode;
     cout << currentNode/rows << " " << currentNode%columns << endl;
+    //this is to get the path from start node to end node
     while(currentNode != (row1*rows + column1)){
-        currentNode = pathCost[currentNode];
+        currentNode = path[currentNode];
+        //cout << "added " << mapVals[currentNode] << endl;
         totalCost+= mapVals[currentNode];
         cout << currentNode/rows << " " << currentNode%columns << endl;
     }
@@ -134,7 +148,6 @@ int main(int argc, char *argv[]) {
         }
         cout << endl;
     }*/
-    
     //prints nodes and the edges they have connections to in the graph
     /*for (const auto& outerpair : graphList) {
         cout << outerpair.first << "| ";
@@ -143,9 +156,6 @@ int main(int argc, char *argv[]) {
         }
         cout << endl;
     }*/   
-
-    //just to do the test get rid of this or edit it
-    
     return 0;
 }
 
